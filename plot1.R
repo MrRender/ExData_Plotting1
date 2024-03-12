91184acd231cc4c3afa93803360cca9761c6f5f8
MrRender <163138108+MrRender@users.noreply.github.com>
2024-03-12 18:47:10 +0200

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,1 +1,19 @@
-
+data <- read.table("C:/Users/yuval/Desktop/R/ex.1/household_power_consumption.txt", header = TRUE, na.strings = "?", sep = ";")
+> data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+> View(data)
+> data$Time <- strptime(data$Time, format = "%H:%M:%S")
+> subset_data <- subset(data, Date >= "2007-02-01" & Date <= "2007-02-02")
+> hist(subset_data$Global_active_power, 
+      col = "red", 
+      xlab = "Global Active Power (kilowatts)", 
+      ylab = "Frequency",
+      main = "Global Active Power"
+ )
+> png("plot1.png", width = 480, height = 480)
+> hist(subset_data$Global_active_power, 
+      col = "red", 
+      xlab = "Global Active Power (kilowatts)", 
+      ylab = "Frequency",
+      main = "Global Active Power"
+ )
+> dev.off()
